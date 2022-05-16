f045ad36bb56f244d1f33a24ce3ea4e12c8edcd5
Brunoaparecido19 <ap24201@icloud.com>
2022-05-16 07:33:07 -0300

    create promodoro-timer and useInterval (React Hook)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { PomodoroTimer } from './components/pomodoro-timer';
 
 function App(): JSX.Element {
-  return <div className="App">helo word</div>;
+  return (
+    <>
+      <div className="App">helo word</div>
+      <PomodoroTimer defaultPomodoroTimer={1500} />
+    </>
+  );
 }
 
 export default App;
