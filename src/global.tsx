0555ca8f9b6e7327fe0746ef2b68189855b775f1
Brunoaparecido19 <ap24201@icloud.com>
2022-05-16 07:10:08 -0300

    create new app

---
+++ src/global.tsx
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  outline: none;
+  scroll-behavior: smooth;
+}
+body {
+  font-family: sans-serif;
+}
+`;
