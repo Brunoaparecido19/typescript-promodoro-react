9ad381f018e11916cf4caf5b558916b8e9f13dec
Brunoaparecido19 <ap24201@icloud.com>
2022-05-17 07:08:18 -0300

    intregando mainTime a duration do CountdownCircleTimer

--- src/components/timer/timer.tsx
+++ src/components/timer/timer.tsx
@@ -1,24 +1,34 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
 import { secondsToTime } from '../../utils/seconds-to-time';
-
 import { CountdownCircleTimer } from 'react-countdown-circle-timer';
+import { Tm, ContainerTm } from './style';
+import { useInterval } from '../../hooks/use-interval';
 
 interface Props {
   mainTime: number;
 }
 
 export function Timer(props: Props): JSX.Element {
+  const [timeDuration, setTimeDuration] = React.useState(props.mainTime);
+  useInterval(() => {
+    setTimeDuration(timeDuration);
+  }, 1000);
+
   return (
     <>
-      <CountdownCircleTimer
-        isPlaying
-        duration={props.mainTime}
-        colors={['#004777', '#F7B801', '#A30000', '#A30000']}
-        colorsTime={[50, 40, 30, 20]}
-        size={550}
-      >
-        {({ remainingTime }) => <div>{secondsToTime(remainingTime)}</div>}
-      </CountdownCircleTimer>
+      <ContainerTm>
+        <CountdownCircleTimer
+          isPlaying
+          size={400}
+          duration={timeDuration}
+          colors={['#2fff00', '#F7B801', '#ff0000', '#A30000']}
+          colorsTime={[10, 6, 3, 0]}
+          strokeWidth={10}
+        >
+          {({ remainingTime }) => <Tm>{secondsToTime(props.mainTime)}</Tm>}
+        </CountdownCircleTimer>
+      </ContainerTm>
     </>
   );
 }
