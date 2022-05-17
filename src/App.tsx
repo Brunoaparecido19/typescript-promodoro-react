bd93d9d319216cb1e4d51c8872d4985f2e0662f0
Brunoaparecido19 <ap24201@icloud.com>
2022-05-17 07:08:38 -0300

    addd GlobablStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { PomodoroTimer } from './components/pomodoro-timer';
+import { PomodoroTimer } from './components/pomodoro-timer/pomodoro-timer';
+import { GlobalStyle } from './global';
 
 function App(): JSX.Element {
   return (
     <>
-      <div className="App">helo word</div>
-      <PomodoroTimer defaultPomodoroTimer={60} />
+      <div className="App">
+        <GlobalStyle></GlobalStyle>
+        <PomodoroTimer defaultPomodoroTimer={10} />
+      </div>
     </>
   );
 }
