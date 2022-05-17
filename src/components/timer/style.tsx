711f91f4cc6c4f06ef92d3ece46445a6e7473f88
Brunoaparecido19 <ap24201@icloud.com>
2022-05-17 07:11:35 -0300

    Create Container of timer

---
+++ src/components/timer/style.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Tm = styled.div`
+  font-size: 5rem;
+  font-family: 'Gruppo', cursive;
+  font-weight: bold;
+  color: #919191;
+`;
+export const ContainerTm = styled.div`
+  background: #333b44;
+  border-radius: 100%;
+  box-shadow: inset 9.91px 15px 15px #17191c, inset -9.91px -9.91px 15px #2b323a;
+  padding: 30px;
+`;
