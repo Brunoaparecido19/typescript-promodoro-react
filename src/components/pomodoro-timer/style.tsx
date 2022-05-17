c4ba15a3f73de28ea9e62e4c4c05fae26c051cb3
Brunoaparecido19 <ap24201@icloud.com>
2022-05-17 07:11:08 -0300

    Create Container and section of pomodoro

---
+++ src/components/pomodoro-timer/style.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const PomodoroSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+export const PomodoroContainer = styled.div`
+  border-radius: 50px;
+  background: #333b44;
+  box-shadow: 20px 20px 60px #2b323a, -20px -20px 60px #3b444e;
+  padding: 35px;
+`;
