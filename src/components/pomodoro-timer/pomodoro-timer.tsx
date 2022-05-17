efcce6fc5b6c58540537423fe5c1d5f807411bdf
Brunoaparecido19 <ap24201@icloud.com>
2022-05-17 16:07:35 -0300

    add configs pause and start of time and animation

--- src/components/pomodoro-timer/pomodoro-timer.tsx
+++ src/components/pomodoro-timer/pomodoro-timer.tsx
@@ -1,30 +1,60 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useInterval } from '../../hooks/use-interval';
 import { secondsToTime } from '../../utils/seconds-to-time';
 import { Button } from '../button/buttom';
 import { Timer } from '../timer/timer';
-import { PomodoroSection } from './style';
+import { PomodoroMain } from './style';
 import { PomodoroContainer } from './style';
 
 interface Props {
   defaultPomodoroTimer: number;
+  shortResttime: number;
+  longRestTime: number;
+  cycles: number;
 }
 export function PomodoroTimer(props: Props): JSX.Element {
   const [mainTime, setMainTime] = React.useState(props.defaultPomodoroTimer);
+  const [timeCounting, setTimeCounting] = React.useState(false);
+  const [working, setWorking] = React.useState(false);
 
-  useInterval(() => {
-    setMainTime(mainTime - 1);
-  }, 1000);
+  useEffect(() => {
+    if (working) document.body.classList.add('working');
+  }, [working]);
+  useInterval(
+    () => {
+      setMainTime(mainTime - 1);
+    },
+    timeCounting ? 1000 : null,
+  );
+
+  const configureWork = (): void => {
+    setTimeCounting(true);
+    setWorking(true);
+  };
 
   return (
     <>
-      <PomodoroSection>
-        <PomodoroContainer>
+      <PomodoroMain className="section-pomodoro">
+        <PomodoroContainer className="container-pomodoro">
           <h2>You are: working</h2>
           <Timer mainTime={mainTime} />
-          <Button text="Work" onClick={() => console.log(1)} />
+          <Button
+            className={'btn-work'}
+            text="Work"
+            onClick={() => configureWork()}
+          />
+          <Button
+            className={'btn-pause'}
+            text="Pause"
+            onClick={() => setTimeCounting(false)}
+          />
+          <Button
+            className={'btn'}
+            text="Reset"
+            onClick={() => console.log(1)}
+          />
         </PomodoroContainer>
-      </PomodoroSection>
+      </PomodoroMain>
     </>
   );
 }
--- src/components/timer/timer.tsx
+++ src/components/timer/timer.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React from 'react';
+import React, { useEffect } from 'react';
 import { secondsToTime } from '../../utils/seconds-to-time';
 import { CountdownCircleTimer } from 'react-countdown-circle-timer';
 import { Tm, ContainerTm } from './style';
@@ -11,6 +11,22 @@ interface Props {
 
 export function Timer(props: Props): JSX.Element {
   const [timeDuration, setTimeDuration] = React.useState(props.mainTime);
+  const [onAnimation, setOnAnimation] = React.useState(false);
+
+  useEffect(() => {
+    const working = document.querySelector('.btn-work') as HTMLElement;
+    working.addEventListener('click', () => {
+      setOnAnimation(true);
+    });
+  }, [onAnimation]);
+
+  useEffect(() => {
+    const pause = document.querySelector('.btn-pause') as HTMLElement;
+    pause.addEventListener('click', () => {
+      setOnAnimation(false);
+    });
+  }, [onAnimation]);
+
   useInterval(() => {
     setTimeDuration(timeDuration);
   }, 1000);
@@ -19,12 +35,12 @@ export function Timer(props: Props): JSX.Element {
     <>
       <ContainerTm>
         <CountdownCircleTimer
-          isPlaying
+          isPlaying={onAnimation}
           size={400}
           duration={timeDuration}
-          colors={['#2fff00', '#F7B801', '#ff0000', '#A30000']}
+          colors={['#404040', '#F7B801', '#ff0000', '#A30000']}
           colorsTime={[10, 6, 3, 0]}
-          strokeWidth={10}
+          strokeWidth={5}
         >
           {({ remainingTime }) => <Tm>{secondsToTime(props.mainTime)}</Tm>}
         </CountdownCircleTimer>
