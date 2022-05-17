d15d848a493f5a1bab45e96e21362b47f5fa29fe
Brunoaparecido19 <ap24201@icloud.com>
2022-05-17 16:08:03 -0300

    new Main style

--- src/components/pomodoro-timer/style.tsx
+++ src/components/pomodoro-timer/style.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const PomodoroSection = styled.section`
+export const PomodoroMain = styled.main`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -8,7 +8,7 @@ export const PomodoroSection = styled.section`
   height: 100vh;
 `;
 
-export const PomodoroContainer = styled.div`
+export const PomodoroContainer = styled.section`
   border-radius: 50px;
   background: #333b44;
   box-shadow: 20px 20px 60px #2b323a, -20px -20px 60px #3b444e;
