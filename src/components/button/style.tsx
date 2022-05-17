f8c9236fc94c5834d4f669b96f2cb38e9a67088e
Brunoaparecido19 <ap24201@icloud.com>
2022-05-17 07:09:30 -0300

    add Style Btn

--- src/components/button/style.tsx
+++ src/components/button/style.tsx
@@ -1,16 +1,30 @@
 import styled from 'styled-components';
 
 export const Btn = styled.button`
+  background-image: linear-gradient(
+    to right,
+    #093028 0%,
+    #237a57 51%,
+    #093028 100%
+  );
+  margin: 10px;
+  padding: 15px 45px;
   text-align: center;
-  font-size: 14px;
-  padding: 10px;
-  border-radius: 7px;
-  box-shadow: 5px -2px 8px #202020;
+  text-transform: uppercase;
+  transition: 0.5s;
+  background-size: 200% auto;
+  color: #c4c4c4;
+  box-shadow: 0 0 20px #2b323a;
+  border-radius: 10px;
+  display: block;
   border: none;
-  :hover {
-    cursor: pointer;
-    background-color: #202020;
+  width: 100%;
+  margin-top: 30px;
+
+  &:hover {
+    background-position: right center; /* change the direction of the change here */
     color: #fff;
-    transition: 0.6s;
+    text-decoration: none;
+    cursor: pointer;
   }
 `;
