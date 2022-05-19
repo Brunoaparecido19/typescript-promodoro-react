3d1819325ac6edcf7d94d7f08cfeea51edf4629d
Brunoaparecido19 <ap24201@icloud.com>
2022-05-19 20:07:34 -0300

    add news styles

--- src/components/pomodoro-timer/style.tsx
+++ src/components/pomodoro-timer/style.tsx
@@ -14,3 +14,28 @@ export const PomodoroContainer = styled.section`
   box-shadow: 20px 20px 60px #2b323a, -20px -20px 60px #3b444e;
   padding: 35px;
 `;
+export const Details = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+  background: #333b44;
+  border-radius: 15%;
+  box-shadow: inset 9.91px 9.91px 15px #2e363e,
+    inset -9.91px -9.91px 15px #38404a;
+`;
+export const DescriptionDetails = styled.p`
+  color: #d5d5d5;
+  font-size: 20px;
+  font-weight: bold;
+  margin-bottom: 10px;
+  margin-top: 20px;
+`;
+export const Title = styled.h1`
+  color: #d5d5d5;
+  font-size: 40px;
+  font-weight: bold;
+  margin-bottom: 40px;
+  text-align: center;
+`;
